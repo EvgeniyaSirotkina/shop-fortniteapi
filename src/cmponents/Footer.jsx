70b5247e591ad1a70441b8213173ab25d83e5de4
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-04 12:52:49 +0300

    format documents

--- src/cmponents/Footer.jsx
+++ src/cmponents/Footer.jsx
@@ -1,19 +1,19 @@
 const Footer = () => {
-    return (  
-        <footer className='page-footer orange accent-1'>
-          <div className='footer-copyright orange accent-1'>
-            <div className='container black-text text-darken-2 '>
-            © {new Date().getFullYear()} All rights reserved
-            <a 
-              className='black-text text-darken-2 right' 
-              href='https://github.com/EvgeniyaSirotkina?tab=repositories' 
-              target='_blank'
-              rel='noreferrer'
-            >GitHub</a>
-            </div>
-          </div>
-        </footer>
-    );
+  return (
+    <footer className='page-footer orange accent-1'>
+      <div className='footer-copyright orange accent-1'>
+        <div className='container black-text text-darken-2 '>
+          © {new Date().getFullYear()} All rights reserved
+          <a
+            className='black-text text-darken-2 right'
+            href='https://github.com/EvgeniyaSirotkina?tab=repositories'
+            target='_blank'
+            rel='noreferrer'
+          >GitHub</a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/cmponents/Header.jsx
+++ src/cmponents/Header.jsx
@@ -1,19 +1,19 @@
 const Header = () => {
-    return (  
+    return (
         <nav className='orange accent-3'>
             <div className='nav-wrapper'>
-            <a href='#' className='brand-logo right'>React Shop</a>
-            <ul id='nav-mobile' className='left hide-on-med-and-down'>
-                <li>
-                    <a 
-                        href='https://github.com/EvgeniyaSirotkina?tab=repositories' 
-                        target='_blank' 
-                        rel='noreferrer'
-                    >
-                        GitHub
-                    </a>
-                </li>
-            </ul>
+                <a href='#' className='brand-logo right'>React Shop</a>
+                <ul id='nav-mobile' className='left hide-on-med-and-down'>
+                    <li>
+                        <a
+                            href='https://github.com/EvgeniyaSirotkina?tab=repositories'
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            GitHub
+                        </a>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
