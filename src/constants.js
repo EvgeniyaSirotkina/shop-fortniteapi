0414d036b74734eb8283921d4671a1c9a4f7134d
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-06 02:09:07 +0300

    add constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const GET_GOODS = 'GET_GOODS';
+
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+
+export const INC_QUANTITY = 'INC_QUANTITY';
+export const DEC_QUANTITY = 'DEC_QUANTITY';
+
+export const CLOSE_ALERT = 'CLOSE_ALERT';
+
+export const TOGGLE_CART = 'TOGGLE_CART';
